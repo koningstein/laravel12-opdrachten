8c3fc601b0632bcc0c990ba96a7eb8cd6fb10173
Koningstein <m.koningstein@tcrmbo.nl>
2025-06-24 12:21:42 +0200

    Update opdracht 13 test naar livewire login

--- tests/Feature/Opdrachten/Opdracht13Test.php
+++ tests/Feature/Opdrachten/Opdracht13Test.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\User;
+use App\Livewire\Auth\Login;
+use Livewire\Livewire;
 
 beforeEach(function (){
     $this->seed('RoleAndPermissionSeeder');
@@ -10,24 +12,39 @@ beforeEach(function (){
 test('the student user can login with email and correct password', function()
 {
     $this->withoutExceptionHandling();
-    $user = User::find(1);
-    $this->post('/login', ['email' => $user->email, 'password' => 'student']);
+
+    $response = Livewire::test(Login::class)
+        ->set('email', 'student@school.nl')
+        ->set('password', 'student')
+        ->call('login');
+
+    $response->assertHasNoErrors();
     $this->assertAuthenticated();
 })->group('Opdracht13');
 
 test('the teacher user can login with email and correct password', function()
 {
     $this->withoutExceptionHandling();
-    $user = User::find(2);
-    $this->post('/login', ['email' => $user->email, 'password' => 'teacher']);
+
+    $response = Livewire::test(Login::class)
+        ->set('email', 'teacher@school.nl')
+        ->set('password', 'teacher')
+        ->call('login');
+
+    $response->assertHasNoErrors();
     $this->assertAuthenticated();
 })->group('Opdracht13');
 
 test('the admin user can login with email and correct password', function()
 {
     $this->withoutExceptionHandling();
-    $user = User::find(3);
-    $this->post('/login', ['email' => $user->email, 'password' => 'admin']);
+
+    $response = Livewire::test(Login::class)
+        ->set('email', 'admin@school.nl')
+        ->set('password', 'admin')
+        ->call('login');
+
+    $response->assertHasNoErrors();
     $this->assertAuthenticated();
 })->group('Opdracht13');
 
